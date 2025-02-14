1b3a3cde65e825f7e5f2ff7357681f0a99a7bffb
Anaba <persideanaba@MacBookAir.fios-router.home>
2025-02-14 09:35:15 -0500

    initiate database micgation in docker file

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
-# Exit on any error
 set -e
 
-# Initialize the migrations directory if it doesn't exist
-if [ ! -d "/app/migrations" ]; then
-    flask db init
-fi
+source /app/.venv/bin/activate
 
-# Create an initial migration if none exists
-if [ -z "$(ls -A /app/migrations/versions)" ]; then
-    flask db migrate -m "Initial migration"
-fi
-
-# Apply the migrations
-flask db upgrade
-
-# Start Flask application using the correct entry point
-exec gunicorn --bind :5000 --workers 1 --threads 4 run:app  # Use Gunicorn in production
+exec gunicorn --bind :5000 --workers 1 --threads 4 run:app
