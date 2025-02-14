ce6c5f896991022ff02cff937a545fc5bfa53722
Anaba <persideanaba@MacBookAir.fios-router.home>
2025-02-14 10:01:00 -0500

    updated sourceb

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
+# Activate the virtual environment
 source /app/.venv/bin/activate
 
+# Start Flask application using Gunicorn
 exec gunicorn --bind :5000 --workers 1 --threads 4 run:app
