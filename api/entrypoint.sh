fafde8ad174162c025a8b2e59c0d83188ce6cb3f
Anaba <persideanaba@MacBookAir.fios-router.home>
2025-02-14 09:14:31 -0500

    improved entrypoint to includ db migration

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -2,7 +2,10 @@
 # Exit on any error
 set -e
 
+# Run database migrations
+flask db init || true
+flask db migrate -m "Initial migration" || true
+flask db upgrade
+
 # Start Flask application using the correct entry point
 exec gunicorn --bind :5000 --workers 1 --threads 4 run:app  # Use Gunicorn in production
-# OR (for development/debugging only)
-# exec python run.py
